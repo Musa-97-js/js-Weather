3689f61bc1ff8c9d8e9b03b4c56f34c8a37fe549
Musa-97-js <aliyevmusa97@gmail.com>
2026-02-18 12:19:22 +0000

    app.js

--- app.js
+++ app.js
@@ -20,21 +20,21 @@ $(document).ready(function () {
         }
     })
 
-    // $('#btn').click(function (e) {
-    //     e.preventDefault()
-    //     let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=SENIN_API_KEY&units=metric`
-    //     async function myweather() {
-    //         let responce = await fetch(url)
-    //         let data = await responce.json()
-    //         $("#city").html(data.name)
-    //         $("#temp").html(data.main.temp)
-    //         $("#feels").html(data.main.feels_like)
-    //         $("#myWeather").val("")
-    //         console.log(data)
-    //     }
-    //     myweather()
+    $('#btn').click(function (e) {
+        e.preventDefault()
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("myWeather").val()}&appid=SENIN_API_KEY&units=metric`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
+        }
+        myweather()
 
-    // })
+    })
 
 
 
